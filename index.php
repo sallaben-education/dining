4fb756a9ffaee28884be676b60d8b842632a2526
sallaben <steven.allaben@gmail.com>
2018-04-23 15:59:10 -0400

    basic structure (header, content, css)

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Project</title>
+</head>
+<body>
+<header>
+    <div class="left">
+        <div class="logo">
+            <a href="./index.php">Project</a>
+        </div>
+    </div>
+    <div class="right">
+        <?php
+            // check if logged in or not, output correct text (does not work yet!)
+            echo '<div class="login"><a href="./login.php">Login</a></div>';
+        ?>
+    </div>
+</header>
+<div class="content">
+</div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,29 @@
+<?php
+    // check if logged in or not. if logged in, send them back to the homepage! (does not work yet!)
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Project - Login</title>
+</head>
+<body>
+<header>
+    <div class="left">
+        <div class="logo">
+            <a href="./index.php">Project</a>
+        </div>
+    </div>
+    <div class="right">
+        <div class="login active">
+            <a href="./login.php">Login</a>
+        </div>
+    </div>
+</header>
+<div class="content">
+</div>
+</body>
+</html>
