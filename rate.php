c141f552927357012b2cd112c47a2faed81dbf39
sallaben <steven.allaben@gmail.com>
2018-04-29 18:56:31 -0400

    Leave a rating!

--- rate.php
+++ rate.php
@@ -46,11 +46,53 @@ SQL;
 </header>
 <div class="content">
     <h1>Rate <?php echo $dining['Name']; ?></h1>
+    <div>Please rate each aspect of this dining hall from 1 star (terrible) to 5 stars (excellent).</div><br>
     <form action="./script/rate.php" method="post">
         <input type="hidden" name="diningID" value="<?php echo $diningID ?>" />
-        <label for="name">Total Rating:</label>
-
-        <input type="text" name="name" id="name">
+        <table class="ratingtable">
+        <tr><th></th><th>1&rarr;2&rarr;3&rarr;4&rarr;5</th></tr>
+        <tr><td><label for="food">Food Quality:</label></td><td>
+        <input type="radio" name="food" value="1">
+        <input type="radio" name="food" value="2">
+        <input type="radio" name="food" value="3" checked>
+        <input type="radio" name="food" value="4">
+        <input type="radio" name="food" value="5">
+        </td></tr>
+        <tr><td><label for="staff">Staff/Service Quality:</label></td><td>
+        <input type="radio" name="staff" value="1">
+        <input type="radio" name="staff" value="2">
+        <input type="radio" name="staff" value="3" checked>
+        <input type="radio" name="staff" value="4">
+        <input type="radio" name="staff" value="5">
+        </td></tr>
+        <tr><td><label for="price">Price/Value:</label></td><td>
+        <input type="radio" name="price" value="1">
+        <input type="radio" name="price" value="2">
+        <input type="radio" name="price" value="3" checked>
+        <input type="radio" name="price" value="4">
+        <input type="radio" name="price" value="5">
+        </td></tr>
+        <tr><td><label for="clean">Cleanliness:</label></td><td>
+        <input type="radio" name="clean" value="1">
+        <input type="radio" name="clean" value="2">
+        <input type="radio" name="clean" value="3" checked>
+        <input type="radio" name="clean" value="4">
+        <input type="radio" name="clean" value="5">
+        </td></tr>
+        <tr><td><label for="speed">Speed:</label></td><td>
+        <input type="radio" name="speed" value="1">
+        <input type="radio" name="speed" value="2">
+        <input type="radio" name="speed" value="3" checked>
+        <input type="radio" name="speed" value="4">
+        <input type="radio" name="speed" value="5">
+        </td></tr>
+        <tr><td><label for="comment">Comment:</label</td><td>
+        <textarea name="comment" id="comment"></textarea>
+        </td></tr>
+        <tr><td><label for="anonymous">Display your name next to this rating:</label></td><td>
+        <input type="radio" name="anonymous" value="false" checked>Yes
+        <input type="radio" name="anonymous" value="true">No
+        </td></tr></table>
         <br>
         <br>
         <input type="submit" value="Submit Rating">
