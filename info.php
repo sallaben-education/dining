7338ba249a828b7d5471eae7d5946c7ac273abca
William Wilson <wwils11@u.rochester.edu>
2018-04-29 13:46:53 -0400

    CreateRating
    CreateRating page added

--- info.php
+++ info.php
@@ -18,7 +18,7 @@ $diningID = strip_tags(urldecode($_GET['id']));
 <header>
     <div class="left">
         <div class="logo">
-            <a href="./index.php">Dining Disaster</a>
+            <a href="./index.php">RateMyDining</a>
         </div>
     </div>
     <div class="right">
@@ -89,6 +89,32 @@ SQL;
   }
   $row = $result->fetch_assoc();
   echo "<h1>" . $row['Name'] . "</h1>School: " . $row['SchoolName'] . "<br>Price: " . $row['Price'];
+
+$_SESSION['diningid'] = $row['Name'];
+if (isset($_SESSION['valid'])) {
+  echo "<br><a href='./createRating.php'>Create Rating</a>";
+}
+$sql = <<<SQL
+    SELECT *
+    FROM Food, FoodType
+    WHERE Food.DiningID = "{$diningID}" AND Food.FoodName = FoodType.FoodName;
+SQL;
+  if(!$result = $db->query($sql)) {
+      die("There was an error running the query [" . $db->error . "]");
+  }
+$i = 1;
+if($result->num_rows > 0) {
+  echo "<table class='spacedtable'><tr><th>#</th><th>Food</th><th>FoodType</th><th>Price</th></tr>";
+}
+while($row = $result->fetch_assoc()) {
+  echo "<tr><td>{$i}</td><td>" . $row['FoodName'] . "</td><td>" . $row['Type'] . "</td><td>" . $row['Price'] . "</td></tr>";
+  $i++;
+}
+if($i == 1) {
+  echo "<br>No matching results!";
+} else {
+  echo "</table>";
+}
 }
 ?>
 </div>
